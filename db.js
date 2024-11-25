c0e03cc3cbe95339079382a44d52ad38684ecf0e
Talha-Talha2 <talha.talha2@education.nsw.gov.au>
2024-11-25 22:12:11 +1100

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 import { collection, onSnapshot } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
-// Listen to the "Artists" collection
+// Access the "Artists" collection
 const artistsCollection = collection(db, "Artists");
+
+// Listen to changes in the collection
 onSnapshot(artistsCollection, (snapshot) => {
   snapshot.docChanges().forEach((change) => {
-    console.log(change);
+    console.log(change.type, change.doc.data());
   });
 });
