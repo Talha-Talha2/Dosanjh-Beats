f6faab1d4eeca5b47e0ea7a105fb8b715e44dca0
Talha-Talha2 <talha.talha2@education.nsw.gov.au>
2024-11-26 21:51:31 +1100

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 // real-time listener
 db.collection('Artists').onSnapshot(snapshot => {
   snapshot.docChanges().forEach(change => {
-    if(change.type === 'added''){
+    if(change.type === 'added'){
       renderArtists(change.doc.data(), change.doc.id);
     }
-    if(change.type === 'removed''){
+    if(change.type === 'removed'){
       renderArtists(change.doc.data(), change.doc.id);
     }
   });
