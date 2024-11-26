4ba8ea06a2007f1b1515d41763bdbc0ee13167b1
Talha-Talha2 <talha.talha2@education.nsw.gov.au>
2024-11-27 02:48:07 +1100

    Update db.js

--- db.js
+++ db.js
@@ -1,4 +1,3 @@
-// real-time listener
 db.collection('Artists').onSnapshot(snapshot => {
   snapshot.docChanges().forEach(change => {
     if(change.type === 'added'){
@@ -8,6 +7,43 @@ db.collection('Artists').onSnapshot(snapshot => {
       renderArtists(change.doc.data(), change.doc.id);
     }
   });
+});
+
+//add new artist
+const artistform = document.querySelector(ADDARTISTFORM);
+artistform.addEventListener('submit', evt => {
+  evt.preventDefault();
+
+  const addartist = {
+    Name: artistform.Name.value,
+    Description: artistform.ArtistDescription.value,
+    Album1: artistform.ALBUM1.value,
+    Album2: artistform.ALBUM2.value,
+    Album3: artistform.ALBUM3.value,
+    Album4: artistform.ALBUM4.value,
+    Album5: artistform.ALBUM5.value,
+    Track1: artistform.TRACK1.value,
+    Track2: artistform.TRACK2.value,
+    Track3: artistform.TRACK3.value,
+    Track4: artistform.TRACK4.value,
+    Track5: artistform.TRACK5.value,
+  };
+
+  db.collection(Artists).add(addartist)
+    .catch(err => console.log(err));
+
+  artistform.Name.value = '';
+  artistform.ArtistDescription.value,
+  artistform.ALBUM1.value = '';
+  artistform.ALBUM2.value = '';
+  artistform.ALBUM3.value = '';
+  artistform.ALBUM4.value = '';
+  artistform.ALBUM5.value = '';
+  artistform.TRACK1.value = '';
+  artistform.TRACK2.value = '';
+  artistform.TRACK3.value = '';
+  artistform.TRACK4.value = '';
+  artistform.TRACK5.value = '';
 });
 
 db.collection('Albums').onSnapshot(snapshot => {
