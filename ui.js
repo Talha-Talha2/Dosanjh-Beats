3a8aa6d398e122cc5de3fccaf2e57f0b1b402060
Talha-Talha2 <talha.talha2@education.nsw.gov.au>
2024-11-27 00:11:55 +1100

    Update ui.js

--- ui.js
+++ ui.js
@@ -5,10 +5,10 @@ const renderArtists = (data, id) => {
   const ARTISTSHTML = `
     <div class="ARTISTTEMPLETE" data-id="${id}">
       <img scr="">
-
-		  <button class="delete" data-id="${id}">🗑️</button>
 		
-		  <h2>${data.Name}</h2>
+		  <button class="delete" data-id="${id}">🗑️</button>
+    
+    		  <h2>${data.Name}</h2>
 
 		  <!-- This is the Artist Description! -->
 		  <p>${data.Description}</p>
@@ -36,4 +36,42 @@ const renderArtists = (data, id) => {
   `;
 
   ARTISTSECTION.innerHTML += ARTISTSHTML
+};
+
+const ALBUMSSECTION = document.querySelector('.ALBUMSSECTION');
+
+//render recipe data
+const renderAlbums = (data, id) => {
+  const ALBUMSHTML = `
+    <div class="ALBUMTEMPLETE">
+    	<img scr="">
+     		
+       		<button class="delete">🗑️</button>
+		
+		<h2>${data.Artist}</h2>
+		
+		<!-- This is the Artist's music Description! -->
+		
+		<p>${data.MusicDescription}</p>
+		
+		<!-- This is the artist's 10 albums! -->
+		
+		<div class="ALBUMS">
+			<h3>Top 10 Albums</h3>
+			<p>${data.Album-1}</p>
+			<p>${data.Album-2}</p>
+			<p>${data.Album-3}</p>
+			<p>${data.Album-4}</p>
+			<p>${data.Album-5}</p>
+   			<p>${data.Album-6}</p>
+			<p>${data.Album-7}</p>
+			<p>${data.Album-8}</p>
+			<p>${data.Album-9}</p>
+			<p>${data.Album-10}</p>
+		</div>
+	
+	</div>
+  `;
+
+  ALBUMSSECTION.innerHTML += ALBUMSHTML
 };
